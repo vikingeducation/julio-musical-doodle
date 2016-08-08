602449545aeb53b55cb0c49dd3c818100b5851cd
Alex Lach <alexglach@gmail.com>
2016-08-08 11:59:36 -0400

    done with assessment

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,5 +1,5 @@
 class Team < ActiveRecord::Base
-  has_many :owner, :class_name => "Employee"
+  belongs_to :owner, :class_name => "Employee"
   has_many :team_memberships
   has_many :members, :through => :team_memberships, :source => :employee
   has_many :progress_reports
