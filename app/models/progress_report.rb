d0278b64d64f112840714c880640397eb5a4b4bb
David Jiang <davidjgrey@gmail>
2016-08-13 13:19:04 -0700

    assessment corrections

--- app/models/progress_report.rb
+++ app/models/progress_report.rb
@@ -1,5 +1,5 @@
 class ProgressReport < ActiveRecord::Base
-  belongs_to :employee
+  belongs_to :author, :class_name => "Employee", :foreign_key => :employee_id 
   belongs_to :team
 
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,5 +1,5 @@
 class Team < ActiveRecord::Base
-  belongs_to :employee
+  belongs_to :owner, :class_name => "Employee", :foreign_key => :employee_id
   has_many :progress_reports
   has_many :memberships
   has_many :members, through: :memberships, source: :employee
