e3b3cec011b4aa262e206d6eb929ba1bf100ceba
“Matthew <matthew.hinea@gmail.com>
2016-08-13 18:41:36 -0700

    create seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+10.times do 
+  employee = Employee.create({
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email
+    })
+  team = employee.teams.create({
+    name: Faker::Team.name
+    })
+  progress_report = employee.progress_reports.create({
+    body: Faker::Lorem.paragraphs(2)
+    })
+  
+end
